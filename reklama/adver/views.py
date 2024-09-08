d67ec59e9596d8ce1f05eca82ddf754ea77f967d
Victus <anvardev888@gmail.com>
2024-09-09 04:49:35 +0500

    last time

--- reklama/adver/views.py
+++ reklama/adver/views.py
@@ -167,6 +167,6 @@ def home(request):
 
 
 def generate_random_string(length):
-    letters = string.ascii_lowercase  # Kichik harflarni tanlash
+    letters = string.ascii_lowercase
     random_string = ''.join(random.choice(letters) for _ in range(length))
     return random_string
